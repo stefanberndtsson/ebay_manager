9c59d8111c2d19082d507d2a482316e9512c620d
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-12-20 20:35:23 +0100

    Some basic prototypes for stuff

--- app/models/imap.rb
+++ app/models/imap.rb
@@ -1,11 +1,30 @@
-class Imap < ActiveRecord::Base
+class Gmail
+  def raw_imap
+    @imap
+  end
+end
+
+class Imap
   class << self
     def connection
       @@gmail ||= nil
-      return @@gmail if @@gmail && @@gmail.logged_in?
+      return @@gmail if @@gmail && @@gmail.logged_in? && !@@gmail.raw_imap.disconnected?
       
+      puts "Connecting"
       @@gmail = Gmail.new(Rails.application.secrets.google_email, Rails.application.secrets.google_password)
       @@gmail
     end
+  end
+
+  def self.labels
+    connection.labels
+  end
+
+  def self.in_label(label)
+    connection.in_label(label)
+  end
+
+  def self.emails_in_label(label)
+      connection.in_label(label).emails
   end
 end
