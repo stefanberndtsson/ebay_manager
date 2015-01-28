b887a73588ebd316ff52daa4e17772a110727649
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2015-01-29 00:14:09 +0100

    Fetch only undelivered items if requesting them

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,6 +1,7 @@
 class ItemController < ApplicationController
   def index
     @items = Item.all
+    @items = @items.where(delivered_at: nil) if params[:undelivered] == "true"
     render json: @items
   end
 end
